9673a02e8c234a9e3c39ca17b488f834932c144b
chdiazguerra <ch.diaz.99@gmail.com>
2023-06-29 00:54:16 -0400

    fix: change timezone for updating state

--- webpage/php/postdata.php
+++ webpage/php/postdata.php
@@ -3,20 +3,23 @@
 $id = $_GET['id'] ?? null;
 $estado = $_GET['estado'] ?? null;
 
-if ($id && $estado){
+if ($id && $estado && ($estado=="1" || $estado=="2" || $estado=="3")){
     $dbServerName = "localhost";
     $dbUserName = "root";
     $dbPassword = "";
     $dbName = "basura";
 
     $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+    
+    date_default_timezone_set("America/Santiago");
+    $date = date('Y-m-d H:i:s');
 
-    $sql = "UPDATE state SET estado='$estado', fecha=CURRENT_TIMESTAMP() WHERE id='$id';";//"INSERT INTO state (id, estado) VALUES ('$id', '$estado') ON DUPLICATE KEY UPDATE estado = '$estado';";
+    $sql = "UPDATE state SET estado='$estado', fecha='$date' WHERE id='$id';";
     
     if ($conn->query($sql) === TRUE) {
         echo "Correct";
       } else {
-        echo "error";//"Error: " . $sql . "<br>" . $conn->error;
+        echo "error " . $conn->error;
       }
 } else {
     echo 'error';
